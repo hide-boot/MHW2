ae18d1dbff612d83629feca31e193ac980a114e5
hide-boot <80949621+hide-boot@users.noreply.github.com>
2021-04-10 19:08:15 +0200

    Add files via upload

---
+++ contents.js
@@ -0,0 +1,82 @@
+const image=[
+    'https://www.farmacosmo.it/13741-large_default/tachipirina-adulti-1000-mg-analgesico-antipiretico-10-supposte-046384.jpg',
+    'https://www.interris.it/wp-content/uploads/2020/02/img800-l-allarme-globale-gli-antibiotici-perdono-efficacia-147806-1280x720.jpg',
+    'https://www.farmaciazanini.it/wp-content/uploads/2018/02/IMG_1960-322x187.jpg',
+    'https://dtbduup7zj5n5.cloudfront.net/catalog/product/cache/6b1c09900b407c50fce2db5e66ebc123/g/a/gaviscon_250ml_24cpr.png',
+    'https://paladinpharma.it/wp-content/uploads/2020/02/sanavita_collirio_Q.png',
+    'https://media.farmae.it/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/f/a/fastumdol_antinfiammatorio_compresse_1_1.jpg',
+    'https://dtbduup7zj5n5.cloudfront.net/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/a/s/aspirina-rapida-10cpr_204920.jpg',
+    'https://image.over-blog.com/PR51WZn8kQo9dpLLOTKw-RAA4TI=/fit-in/500x500/filters:no_upscale()/image%2F2601385%2F20200204%2Fob_9313e1_benzodiazepines-france.jpg',
+    'https://cdn.shop-farmacia.it/images/420x0/cardiol-capsule-morbide-IT913194193-p1.webp',
+    'https://www.greenme.it/wp-content/uploads/2019/03/irbesartan.jpg',
+    'https://www.mypharmaclick.com/img_prodotto/768x768/q/nurofen-antidolorifico-12-cpr-rivestite-400-mg_910.jpg',
+    'https://c8.alamy.com/compit/rhtt0b/chonburi-thailandia-novembre-28-2018-seretide-accuhaler-e-imballaggio-asma-accuhaler-per-il-trattamento-di-asma-broncodilatatori-e-steroidi-farmaco-per-s-rhtt0b.jpg',
+    'https://media.farmae.it/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/a/bagno_schiuma.jpg',
+    'http://www.htcos.it/images/htcosit/613-profumi-equivalenti-in-farmacia-mast-industria-italiana-4858.jpg',
+    'https://www.farmaciasacrocuoretorino.com/wp-content/uploads/2018/08/Shampoo-delicato-800x800.png',
+    'https://static.sorgentenatura.it/data/spec/big/crema-viso-12075.jpg?_=1526894298'
+
+
+];
+
+
+
+const title=[
+'Paracetamolo',
+'Antibiotico',
+'integratore',
+'Gastroprotettore',
+'Collirio',
+'Antinfiammatori',
+'Aspirina',
+'Benzodiazepine',
+'Fluidificanti del sangue',
+'Ipertensione',
+'Antidolorifico',
+'Broncodilatatori',
+'Bagnodoccia',
+'Profumo',
+'Shampoo',
+'Crema'];
+
+
+const prezzo=[
+  '7.49€',
+  '10.50€',
+  '20.60€',
+  '15.45€',
+  '8.30€',
+  '6.20€',
+  '12.79€',
+  '10.20€',
+  '9.49€',
+  '6.50€',
+  '4.60€',
+  '10.45€',
+  '7.30€',
+  '9.20€',
+  '15.79€',
+  '20.20€'
+];
+
+
+  const desrciption=[
+    'è un farmaco ad azione analgesica e antipiretica largamente utilizzato sia da solo sia in associazione ad altre sostanze, ad esempio nei comuni preparati da banco per le forme virali da raffreddamento, o nei farmaci destinati al trattamento del dolore acuto e cronico. I',
+    'sostanza in grado di rallentare o fermare la proliferazione dei batteri. Gli antibiotici si distinguono pertanto in batteriostatici (cioè bloccano la riproduzione del batterio, impedendone la scissione) e battericidi (cioè uccidono direttamente il microrganism',
+    'favorire l assunzione di determinati principi nutritivi',
+    'inibitori della pompa protonica (IPP, impropriamente noti anche come Prazoli) sono un gruppo di molecole la cui azione principale è una pronunciata riduzione di lunga durata (dalle 18 alle 24 ore) dell acidità dei succhi gastrici.',
+    ' è un liquido contenente varie sostanze medicamentose che si applica a gocce in sede oculare per curare le patologie dell occhio',
+    'rimediando al dolore attraverso la riduzione dell infiammazione',
+    ' è un farmaco antinfiammatorio non-steroideo della famiglia dei salicilati',
+    ' sono una classe di psicofarmaci la cui struttura chimica è composta dalla fusione di un anello benzenico e un anello diazepinico.',
+    'è un composto capace di rallentare o interrompere il processo di coagulazione del sangue, agendo sull emostasi secondaria',
+    'sono farmaci che trovano impiego specialmente nella terapia dellipertensione arteriosa, del post-infarto del miocardio e dell insufficienza cardiaca cronica.',
+    'utilizzato per lenire il dolore riducendolo, senza però intervenire sulle cause che l hanno provocato',
+    'utilizzati per l’asma e per le malattie polmonari respiratorie croniche.',
+    'Il Bagno Doccia Tonificante favorisce un’azione stimolante di piacevole benessere sulla pelle; le essenze contenute nelle erbe di montagna arricchite con distillati di equiseto arvense e menta piperita intervengono aiutando a recuperare e ricostruire il tono e l’elasticità della pelle',
+    ' miscela a base di alcool o sostanze oleose, con sostanze odorose',
+    'rimuovere unto, sporco e particelle di pelle, inquinamento ambientale e/o altre particelle inquinanti che gradualmente si formano e depositano sui capelli, così da rendere il capello e il cuoio capelluto puliti    ',
+    'idratazione della pelle di tutto il viso'
+  ];
+
+
---
+++ script.js
@@ -0,0 +1,174 @@
+//prezzo,img1,img2,title,title_two
+
+const preferito=document.querySelector('.preferiti');
+const mainblock=document.querySelector('.mainblock');
+const vendita =document.querySelector('.vendita');
+const input=document.querySelector('input');
+input.addEventListener('keyup',cerca);
+vendita.classList.add('hidden');
+for(let i=0;i<title.length;i++){
+    const div=document.createElement('div');
+    const titolo=document.createElement('h4');
+    const mainimg=document.createElement('img');
+    const costo=document.createElement('p'); 
+    const carrello=document.createElement('img');
+    const descrizione=document.createElement('p');
+    const dettagli=document.createElement('p');
+    
+    carrello.src='https://banner2.cleanpng.com/20180320/hhw/kisspng-shopping-cart-computer-icons-online-shopping-websi-files-free-grocery-cart-5ab09145b5dd11.5012096615215209657449.jpg';
+    dettagli.textContent='Mostra altro';
+    titolo.textContent=title[i];
+    mainimg.src=image[i];
+    costo.textContent='Prezzo:'+prezzo[i];
+    descrizione.textContent=desrciption[i];
+
+
+    div.classList.add('block');
+    titolo.classList.add('titolo');
+    mainimg.classList.add('mainimg');
+    costo.classList.add('testo');
+    carrello.classList.add('classcar');
+    descrizione.classList.add('description');
+    dettagli.classList.add('dettagli');
+    descrizione.classList.add('hidden');
+   
+    
+    div.appendChild(titolo);
+    div.appendChild(mainimg);
+    div.appendChild(costo);
+    div.appendChild(carrello);
+    div.appendChild(descrizione);
+    div.appendChild(dettagli);
+    mainblock.appendChild(div);
+
+    dettagli.addEventListener('click',more);
+    carrello.addEventListener('click',preferiti);
+
+}
+
+
+
+
+ 
+function more(event){
+    const block=document.querySelectorAll('.mainblock .block');
+    const dettagli=document.querySelectorAll('.block .dettagli');
+    const nascondi=document.querySelectorAll('.description');
+
+    for(let i=0;i<block.length;i++){
+        if(dettagli[i].textContent==='Riduci'){
+            dettagli[i].textContent='Mostra Altro';
+            dettagli[i].removeEventListener('click',minus);
+            dettagli[i].addEventListener('click',more);
+            block[i].classList.remove('selezionato');
+            nascondi[i].classList.add('hidden');
+  
+            
+        }
+    }
+   
+    const mostra=event.currentTarget.parentNode.querySelector('.description');
+    mostra.classList.remove('hidden');
+    
+    event.currentTarget.textContent='Riduci';
+    event.currentTarget.parentNode.classList.add('selezionato');
+    event.currentTarget.removeEventListener('click',more);
+    event.currentTarget.addEventListener('click',minus);
+    
+    }
+
+function minus(event){
+
+    
+    event.currentTarget.textContent='Mostra altro';
+    const block=document.querySelectorAll('.mainblock .block');
+    
+    event.currentTarget.removeEventListener('click',minus);
+    event.currentTarget.addEventListener('click',more);
+    const nascondi=event.currentTarget.parentNode.querySelector('.description');
+    event.currentTarget.parentNode.classList.remove('selezionato');
+    nascondi.classList.add('hidden');
+    
+}
+
+
+
+
+
+
+function preferiti(event){
+const div2=document.createElement('div');   
+const titolo2=document.createElement('h4');
+titolo2.textContent=event.currentTarget.parentNode.querySelector('.titolo').textContent;
+const mainimg2=document.createElement('img');
+mainimg2.src=event.currentTarget.parentNode.querySelector('.mainimg').src;
+const costo2=document.createElement('p');
+costo2.textContent=event.currentTarget.parentNode.querySelector('.testo').textContent;
+const carrello2=document.createElement('img');
+carrello2.src='http://www.educational.rai.it/materiali/italiano/immagini_esercizi/Punt7cartelloD.jpg'
+div2.appendChild(titolo2);
+div2.appendChild(mainimg2);
+div2.appendChild(costo2);
+div2.appendChild(carrello2);
+div2.classList.add('block');
+div2.classList.add('standard');
+titolo2.classList.add('titolo');
+mainimg2.classList.add('mainimg');
+carrello2.classList.add('classcar');
+costo2.classList.add('testo');
+carrello2.addEventListener('click',remove);
+preferito.appendChild(div2);
+event.currentTarget.removeEventListener('click',preferiti);
+const blockp=document.querySelectorAll('.preferiti .block');
+
+if(blockp!==null){
+    vendita.classList.remove('hidden');
+}
+}
+
+
+function remove(event){
+    const block=document.querySelectorAll('.block');
+    for(let i=0;i<block.length;i++){
+        if(event.currentTarget.parentNode.querySelector('h4').textContent===block[i].querySelector('.titolo').textContent){
+            block[i].querySelector('.classcar').addEventListener('click',preferiti);
+        }
+    
+    }
+    preferito.removeChild(event.currentTarget.parentNode);
+    event.currentTarget.removeEventListener('click',remove);
+    const blockp= document.querySelector('.preferiti .block');
+    if(blockp===null){
+           vendita.classList.add('hidden');
+    }
+   }
+
+  
+   function cerca(event){
+ const parola=event.target.value.toLowerCase();
+ const block=document.querySelectorAll('.mainblock .block');
+ const titleb=document.querySelectorAll('.titolo');
+let t=0;
+
+for(let i=0;i<block.length;i++){
+     if(titleb[i].textContent.toLowerCase().indexOf(parola)!==-1 ){
+        block[i].classList.remove('hidden');
+      
+         
+        }
+     else{
+        block[i].classList.add('hidden');
+      
+       
+     }
+
+ }  
+        if(parola!==''){
+            mainblock.classList.add('height');
+        }
+     if(parola===''){
+       mainblock.classList.remove('height');
+     }
+
+
+}
